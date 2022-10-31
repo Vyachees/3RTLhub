685ae8c3e5aeea7b294759d4270b2a70e55ec41c
Vyacheslav <kirillov.com@gmail.com>
2022-10-31 23:15:03 +0300

    Add code

---
+++ src/main/java/com/example/RTLhub/RTLhub.java
@@ -0,0 +1,55 @@
+package com.example.RTLhub;
+
+/**
+ * @author Vyacheslav Kirillov
+ * @create 2022.10.31 20:34
+ **/
+
+import java.util.ArrayList;
+import java.util.List;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class RTLhub {
+    // Port where chat server will listen for connections.
+    static final int PORT = 50004;
+@Bean
+    public static void receive() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup) // Set boss & worker groups
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new StringDecoder());
+                            p.addLast(new StringEncoder());
+                            p.addLast(new RTLhubHandler());
+                        }
+                    });
+            // Start the server.
+            ChannelFuture f = b.bind(PORT).sync();
+            System.out.println("Netty Server started.");
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            // Shut down all event loops to terminate all threads.
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/example/RTLhub/RTLhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.RTLhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RTLhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RTLhubApplication.class, args);
+    }
+
+}
